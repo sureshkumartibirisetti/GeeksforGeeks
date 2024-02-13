37199d3a0124138f4b710a631c50c364ddbcaec8
SURESH KUMAR <84790930+sureshkumartibirisetti@users.noreply.github.com>
2024-02-14 00:40:24 +0530

    Easy

---
+++ Taking input - Java.java
@@ -0,0 +1,30 @@
+//User function Template for Java
+
+class Geeks{
+    
+    // Function to take input using Scanner class
+    static void IOFunction(){
+        Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		
+		while(t-- > 0){
+		     int s = sc.nextInt();
+        float a = sc.nextFloat();
+        long b = sc.nextLong();
+        byte c = sc.nextByte();
+        sc.nextLine();
+        String d = sc.nextLine();
+        
+        System.out.println(s);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+		    // Your code here
+		    
+		    
+		}
+		
+    }
+    
+}
