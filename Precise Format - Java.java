73e9455c67e2fe3fd2d079831299f44dfe8f8bf0
SURESH KUMAR <84790930+sureshkumartibirisetti@users.noreply.github.com>
2024-02-14 00:01:00 +0530

    Easy

---
+++ Precise Format - Java.java
@@ -0,0 +1,15 @@
+//User function Template for Java
+
+class Geeks{
+    
+    static void printInFormat(float a, float b){
+        float result = a/b;
+        System.out.print(result+" ");
+        System.out.format("%.3f",result);
+        
+        
+        // Your code here to print upto 3 decimal places
+        
+    }    
+    
+}
