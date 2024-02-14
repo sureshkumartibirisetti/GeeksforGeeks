f0dd65777d0b1f39944bc93053b974e3ffbcdd5c
SURESH KUMAR <84790930+sureshkumartibirisetti@users.noreply.github.com>
2024-02-14 22:51:35 +0530

    Easy

---
+++ Power of 2.java
@@ -0,0 +1,16 @@
+
+
+//User function Template for Java
+
+class Solution{
+    
+    // Function to check if given number n is a power of two.
+    public static boolean isPowerofTwo(long n){
+        
+        // Your code here
+        if(n<=0) return false;
+        return (n & (n-1)) ==0;
+        
+    }
+    
+}
