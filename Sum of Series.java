ba2ca65ac9423d1f7456a07186b37d275009e243
SURESH KUMAR <84790930+sureshkumartibirisetti@users.noreply.github.com>
2024-02-15 23:37:26 +0530

    School

---
+++ Sum of Series.java
@@ -0,0 +1,14 @@
+
+
+
+class Solution {
+    public static long seriesSum(int n) {
+        // code here
+        long ans=0;
+        for(int i=0;i<=n;i++){
+            ans+=i;
+        }
+        return ans;
+    }
+}
+        
